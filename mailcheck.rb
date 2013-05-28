e17b5ab5d2fe33879276d1f9e20503ce84962b93
Roberto Salas <wala.rsalas@gmail.com>
2013-05-28 18:11:18 -0400

    Added color and small fixes

--- mailcheck.rb
+++ mailcheck.rb
@@ -1,28 +1,23 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
 require 'gmail'
 require 'mail'
+require 'colorize'
 
-
-username="email"
-password="password" 
-
+username="mail"
+password="password"
 
 gmail = Gmail.new(username, password)
 
-
-
 new_emails = gmail.inbox.emails(:unread)
-puts "You have #{new_emails.length} new emails."
+puts "You have " + "#{new_emails.length}".colorize( :red ) + " new emails!"
 puts "\n"
 
-
-new_emails.each do |email|
+new_emails.reverse_each do |email|
         email_from = email.from[0].name
-        puts "From: #{email_from}" 
+        puts "From: ".colorize( :blue ) + "#{email_from}" 
         email_subject = Mail::Encodings.value_decode(email.subject)
-        puts "Subject: #{email_subject.tr("\n","").gsub(/\s+/, " ").strip}"
+        puts "Subject: ".colorize( :green ) + "#{email_subject.tr("\n","").gsub(/\s+/, " ").strip}"
         puts "\n"
         end
 
-gmail.logout  
-
+gmail.logout
