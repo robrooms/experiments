34b9ae91385611845cb9326a9584f14834ca2f9d
Roberto Salas <wala.rsalas@gmail.com>
2013-05-28 21:30:07 -0400

    Added a function to mark everything as read

--- mailcheck.rb
+++ mailcheck.rb
@@ -10,14 +10,24 @@ gmail = Gmail.new(username, password)
 
 new_emails = gmail.inbox.emails(:unread)
 puts "You have " + "#{new_emails.length}".colorize( :red ) + " new emails!"
-puts "\n"
 
 new_emails.reverse_each do |email|
+        puts "\n"
         email_from = email.from[0].name
         puts "From: ".colorize( :blue ) + "#{email_from}" 
         email_subject = Mail::Encodings.value_decode(email.subject)
         puts "Subject: ".colorize( :green ) + "#{email_subject.tr("\n","").gsub(/\s+/, " ").strip}"
-        puts "\n"
         end
 
-gmail.logout
+if "#{new_emails.length}" != "0"
+  puts "\n"
+  puts 'Mark all as read (y/N)?'
+  case gets.strip
+    when 'Y', 'y', 'yes'
+   		new_emails.reverse_each do |email|
+	    email.mark(:read)
+	end
+    when /\A[nN]o?\Z/, ''
+      gmail.logout  
+ 	end
+end
